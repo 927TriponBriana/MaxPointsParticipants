0538fe55d0c9a61dfbf0c8c40841e5509ac0a6fc
Briana Tripon <briana.tripon@stud.ubbcluj.ro>
2024-04-02 16:41:23 +0300

    Fixed tests

--- src/test/java/ssvv/example/TestAddStudent.java
+++ src/test/java/ssvv/example/TestAddStudent.java
@@ -10,29 +10,56 @@ import service.Service;
 import repository.StudentXMLRepository;
 import validation.StudentValidator;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class TestAddStudent {
+    private StudentXMLRepository studentFileRepository;
+    private StudentValidator studentValidator;
+    private Service service;
+
+    @BeforeAll
+    static void createXML() {
+        File xml = new File("studentiTest.xml");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(xml))) {
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+                    "<inbox>\n" +
+                    "\n" +
+                    "</inbox>");
+            writer.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeEach
+    public void setup() {
+        this.studentValidator = new StudentValidator();
+        this.studentFileRepository = new StudentXMLRepository(studentValidator, "studentiTest.xml");
+        this.service = new Service(this.studentFileRepository, null, null);
+    }
+
+    @AfterAll
+    static void removeXML() {
+        new File("fisiere/studentiTest.xml").delete();
+    }
 
     @Test
     public void testSaveStudentSuccess() {
-        StudentValidator validator = new StudentValidator();
-        StudentXMLRepository studentRepo = new StudentXMLRepository(validator, "studenti.xml");
-        Service service = new Service(studentRepo, null, null);
-
-        int result = service.saveStudent("1", "Test Student", 101);
+        int result = service.saveStudent("5", "Antonia", 937);
 
         assertEquals(1, result, "Student should be successfully saved and method should return 1");
     }
 
     @Test
     public void testSaveStudentFailureDueToDuplicate() {
-        StudentValidator validator = new StudentValidator();
-        StudentXMLRepository studentRepo = new StudentXMLRepository(validator, "students.xml");
-        Service service = new Service(studentRepo, null, null);
+        service.saveStudent("1", "Ana", 931);
+        int result = service.saveStudent("1", "Ana", 931);
 
-        service.saveStudent("1", "Existing Student", 101);
-        int result = service.saveStudent("1", "New Student", 102);
-
-        assertEquals(1, result, "Attempt to save a duplicate student should fail and return 0");
+        assertEquals(0, result, "Attempt to save a duplicate student should fail and return 0");
     }
 }
