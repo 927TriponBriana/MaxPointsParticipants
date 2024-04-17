aa198263bd7c175221a3fd2aa00e86d49c12e360
Briana Tripon <briana.tripon@stud.ubbcluj.ro>
2024-04-17 13:29:39 +0300

    Solved issues

--- src/test/java/ssvv/example/BigBangTesting.java
+++ src/test/java/ssvv/example/BigBangTesting.java
@@ -1,10 +1,7 @@
 package ssvv.example;
 
-import domain.Nota;
-import domain.Pair;
-import domain.Student;
-import domain.Tema;
 import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,13 +21,97 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class BigBangTesting {
+    //    private Service service;
+//
+//    @BeforeAll
+//    static void createXML() {
+//        File xml = new File("studentiTest.xml");
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(xml))) {
+//            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+//                    "<inbox>\n" +
+//                    "\n" +
+//                    "</inbox>");
+//            writer.flush();
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        File xml2 = new File("temeTest.xml");
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(xml2))) {
+//            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+//                    "<inbox>\n" +
+//                    "\n" +
+//                    "</inbox>");
+//            writer.flush();
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        File xml3 = new File("noteTest.xml");
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(xml3))) {
+//            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+//                    "<inbox>\n" +
+//                    "\n" +
+//                    "</inbox>");
+//            writer.flush();
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @BeforeEach
+//    public void setup() {
+//        StudentValidator studentValidator = new StudentValidator();
+//        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, "studentiTest.xml");
+//        TemaValidator temaValidator = new TemaValidator();
+//        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "temeTest.xml");
+//        NotaValidator notaValidator = new NotaValidator();
+//        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, "noteTest.xml");
+//        service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+//    }
+//
+//    @AfterAll
+//    public static void teardown() {
+//
+//        new File("studentiTest.xml").delete();
+//        new File("temeTest.xml").delete();
+//        new File("noteTest.xml").delete();
+//    }
+//
+//    @Test
+//    public void testAddStudent() {
+//        assertEquals(1, service.saveStudent("1", "Maria", 937));
+//    }
+//
+//    @Test
+//    public void testAddTema() {
+//        assertEquals(1, service.saveTema("1", "A1", 3, 1));
+//    }
+//
+//    @Test
+//    public void testAddGrade() {
+//        assertEquals(1, service.saveNota("1", "1", 9.0, 3, "correct"));
+//
+//        //service.deleteNota("333");
+//        service.deleteStudent("1");
+//        service.deleteTema("1");
+//    }
+//
+//    @Test
+//    public void integrationTest() {
+//        assertEquals(1, service.saveStudent("2", "Ioana", 931));
+//        assertEquals(1, service.saveTema("2", "A2", 4, 2));
+//        assertEquals(1, service.saveNota("2", "2", 9.5, 4, "good"));
+//
+//        //service.deleteNota("333");
+//        service.deleteStudent("1");
+//        service.deleteTema("1");
+//    }
+//}
     private Service service;
-    private TemaXMLRepository temaXMLRepository;
-    private TemaValidator temaValidator;
-    private StudentXMLRepository studentXMLRepository;
-    private StudentValidator studentValidator;
-    private NotaXMLRepository notaXMLRepository;
-    private NotaValidator notaValidator;
 
     @BeforeAll
     static void createXML() {
@@ -41,8 +122,7 @@ public class BigBangTesting {
                     "\n" +
                     "</inbox>");
             writer.flush();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -53,8 +133,7 @@ public class BigBangTesting {
                     "\n" +
                     "</inbox>");
             writer.flush();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         File xml3 = new File("bigBangNotaTest.xml");
@@ -64,26 +143,28 @@ public class BigBangTesting {
                     "\n" +
                     "</inbox>");
             writer.flush();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     @BeforeEach
     public void setup() {
-        this.temaValidator = new TemaValidator();
-        this.temaXMLRepository = new TemaXMLRepository(temaValidator, "bigBangTemaTest.xml");
-        this.studentValidator = new StudentValidator();
-        this.studentXMLRepository = new StudentXMLRepository(studentValidator, "bigBangStudentTest.xml");
-        this.notaValidator = new NotaValidator();
-        this.notaXMLRepository = new NotaXMLRepository(notaValidator, "bigBangNotaTest.xml");
-        this.service = new Service(this.studentXMLRepository, this.temaXMLRepository, this.notaXMLRepository);
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "bigBangTemaTest.xml");
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, "bigBangStudentTest.xml");
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, "bigBangNotaTest.xml");
+        this.service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
     }
 
-    @Before
-    public void setUp() {
-        service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);  // Initialize your service
+    @AfterAll
+    public static void teardown() {
+
+        new File("studentiTest.xml").delete();
+        new File("temeTest.xml").delete();
+        new File("noteTest.xml").delete();
     }
 
     @Test
@@ -102,18 +183,24 @@ public class BigBangTesting {
 
     @Test
     public void testAddGrade() {
-        service.saveStudent("5", "Maria", 937);
+        service.saveStudent("1", "Maria", 937);
         service.saveTema("1", "MPP", 7, 6);
-        assertEquals(-1, service.saveNota("1", "1", 10, 3, "good"));
+        assertEquals(1, service.saveNota("1", "1", 9, 7, "good"));
+
+        service.deleteStudent("1");
+        service.deleteTema("1");
     }
 
     @Test
     public void integrationTest() {
         int result = service.saveStudent("2", "Marian", 937);
-        int resultTema = service.saveTema("1", "MPP", 7, 6);
+        int resultTema = service.saveTema("2", "MPP", 7, 6);
 
         assertEquals(1, result);
         assertEquals(1, resultTema);
-        assertEquals(-1, service.saveNota("2", "2", 10, 3, "good"));
+        assertEquals(1, service.saveNota("2", "2", 10, 7, "good"));
+
+        service.deleteStudent("1");
+        service.deleteTema("1");
     }
 }
